00ba7cedb02b2d7e41fde452b007e426a66f576e
Hugo Stark <hugostark@Hugos-MacBook-Pro.local>
2025-01-05 17:11:22 +0200

    Sort words by count.

--- word-count.py
+++ word-count.py
@@ -11,9 +11,13 @@ def normalizeWords(text):
 
 input = sc.textFile("data/book")
 words = input.flatMap(normalizeWords)
-wordCounts = words.countByValue()
 
-for word, count in wordCounts.items():
-    cleanWord = word.encode("ascii", "ignore")
-    if cleanWord:
-        print(cleanWord.decode() + " " + str(count))
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey()
+results = wordCountsSorted.collect()
+
+for result in results:
+    count = str(result[0])
+    word = result[1].encode("ascii", "ignore")
+    if word:
+        print(word.decode() + ":\t\t" + count)
