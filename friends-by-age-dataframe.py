a12de93906f4c556078ec13dd5fb67cc00a1ff83
Hugo Stark <hugostark@Hugos-MacBook-Pro.local>
2025-01-21 16:59:43 +0200

    Add stuff.

--- friends-by-age-dataframe.py
+++ friends-by-age-dataframe.py
@@ -1,21 +1,30 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, avg
+from pyspark.sql import Row
+from pyspark.sql import functions as func
 
-spark = SparkSession.builder.appName("SparkSQL").getOrCreate()
+spark = SparkSession.builder.appName("FriendsByAge").getOrCreate()
 
-people = (
+lines = (
     spark.read.option("header", "true")
     .option("inferSchema", "true")
     .csv("data/fakefriends-header.csv")
 )
 
-print("Here is our inferred schema:")
-people.printSchema()
+# Select only age and numFriends columns
+friendsByAge = lines.select("age", "friends")
 
-print("Show name and age:")
-people.select(people.name, people.age).show()
+# From friendsByAge we group by "age" and then compute average
+friendsByAge.groupBy("age").avg("friends").show()
 
-print("Show average age:")
-people.select(avg(people.age)).show()
+# Sorted
+friendsByAge.groupBy("age").avg("friends").sort("age").show()
+
+# Formatted more nicely
+friendsByAge.groupBy("age").agg(func.round(func.avg("friends"), 2)).sort("age").show()
+
+# With a custom column name
+friendsByAge.groupBy("age").agg(
+    func.round(func.avg("friends"), 2).alias("friends_avg")
+).sort("age").show()
 
 spark.stop()
